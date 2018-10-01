41e22c482553ad4d931ce799c0382f8277ea2673
Shatur95 <genaloner@gmail.com>
2018-10-01 23:56:26 +0300

    Remove old code

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,17 @@
-#include "mainwindow.h"
 #include <QApplication>
-
 #include <QFile>
 #include <QApplication>
 #include <QtDebug>
 #include <QtGlobal>
 
+#include "mainwindow.h"
 #include "camera/cam_imgpreocessor.h"
 
 
-#ifdef Q_OS_WIN32
-
-static QFile dbg("d:\\fesko_debug.txt");
-
-void myMessageOutput(QtMsgType type, const QMessageLogContext &, const QString & str)
-{
-    dbg.write(str.toStdString().c_str());
-}
-
-#endif
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-#ifdef Q_OS_WIN32
-    dbg.open(QIODevice::Truncate | QIODevice::WriteOnly);
-    //qInstallMessageHandler(myMessageOutput);
-
-#endif
 #if defined (Q_OS_ANDROID) || defined (Q_OS_IOS)
     w.showMaximized();
 #else
